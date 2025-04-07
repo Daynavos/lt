2ca30da814e418bb9f095a6d2265cdf096a3006a
Daynavos <dayna22vos@gmail.com>
2025-04-07 14:14:10 +0200

    test_findTop added

--- test_getbest.py
+++ test_getbest.py
@@ -11,6 +11,21 @@ class TestGetBest(unittest.TestCase):
         self.assertEqual(num_col,1)
         self.assertEqual(mark_col,2)
     
+    def test_findTop(self):
+        sample_data = """Course,Student Number,Mark,Comment
+ELEN3020,160001,72,OK
+ELEN3020,167381,90,Check
+ELEN3020,143211,83,-
+ELEN3020,17171,48,Redo
+ELEN3020,191919,73,-"""
+        
+        dummy_file = StringIO(sample_data)
+        num_col = 1
+        mark_col = 2
+        best_ind, best = getbest.findTop(dummy_file, num_col, mark_col)
+
+        self.assertEqual(best_ind, "167381")
+        self.assertEqual(best, 90)
     
 if __name__ == '__main__':
     unittest.main()
